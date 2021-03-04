7f353da6d5a9e1d9b4233a27fb8a7223728a6df9
mnosel00 <mateusz.nosel@microsoft.wsei.edu.pl>
2021-03-04 16:46:19 +0100

    Pierwsze przesłanie

---
+++ Emerytura/ConsoleAppInterakcja/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleAppInterakcja
+{
+    const int WIEK_EMERYTALNY = 65;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj!");
+            Console.Write("Podaj imię: "); //cw tab tab (od razu leci )
+           string imie = Console.ReadLine(); // zapisanie imienia do zmiennej
+          
+
+            Console.WriteLine("Podaj nazwisko"); //cw tab tab (od razu leci )
+            string nazwisko = Console.ReadLine(); //zapisanie nazwiska
+
+            Console.WriteLine("Witaj "+ imie +""+ nazwisko + "!");
+            Console.WriteLine($"Witaj {imie} {nazwisko} !"); //szablon napisu
+            Console.WriteLine(string.Format("Witaj {0} {1}!",nazwisko,imie));
+
+
+            //zabawa z liczba
+            Console.Write("Podaj Wiek: ");
+            //string napis = Console.ReadLine();
+            int wiek = int.Parse(Console.ReadLine());
+
+            if(wiek >= WIEK_EMERYTALNY)
+            {
+                Console.WriteLine("Jesteś emerytem");
+            }
+            else
+            {
+                Console.WriteLine($"Do emerytury zostało ci {WIEK_EMERYTALNY - wiek} lat.");
+            }
+
+
+
+
+        }
+    }
+}
